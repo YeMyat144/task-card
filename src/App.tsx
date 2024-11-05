780241abfda7cc3dd72c29b67031269f2a7e70aa
YeMyat144 <149359862+YeMyat144@users.noreply.github.com>
2024-11-05 17:13:56 +0700

    Update Netlify configuration and switch to HashRouter for routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { ThemeProvider, createTheme, CssBaseline, Box, Typography, IconButton } from '@mui/material';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Board from './components/Board';
 import Login from './Login';
 import Register from './Register';
